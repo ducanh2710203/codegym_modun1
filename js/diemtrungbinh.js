372a9fcc3091415242f42bba5a93d0d59a289bac
ducanh2710203 <duc2710203@gmail.com>
2023-03-10 16:25:29 +0700

    first commit

--- js/diemtrungbinh.js
+++ js/diemtrungbinh.js
@@ -1,6 +1,6 @@
-let toan = 5 ;
-let ly =   5;
-let hoa =  5;
-    let trungbinh = (Number(toan) +Number(ly) + Number(hoa))/3
-document.write( "điểm trung bình môn là: "+trungbinh+" điểm");
+let toan = 5;
+let ly = 5;
+let hoa = 5;
+let trungbinh = (Number(toan) + Number(ly) + Number(hoa)) / 3
+document.write("điểm trung bình môn là: " + trungbinh + " điểm");
 console.log(trungbinh);
